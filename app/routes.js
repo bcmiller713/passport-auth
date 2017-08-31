d81b5c76f25e6652a2501fb890366036cd961c2f
bcmiller713 <bcmiller713@gmail.com>
2017-08-31 11:06:06 -0700

    passport login post route

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,14 @@ module.exports = function(app, passport) {
   });
 
   // process the login form
-  // app.post("/login", passport stuff here);
+  app.post("/login", passport.authenticate("local-login", {
+    // redirect to the secure profile section
+    successRedirect: "/profile",
+    // redirect back to the singup page if there is an error
+    failureRedirect: "/login",
+    // allow flash messages
+    failureFlash: true
+  }));
 
   //============= SIGNUP =============
   app.get("/signup", function(req, res) {
